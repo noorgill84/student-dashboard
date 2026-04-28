d9c11dece4c4048b486ba070b44896cfcb3bbada
noorgill84 <tejnoors79@gmail.com>
2026-04-28 12:39:51 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Toggle mobile menu
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("show");
+}
+
+// Loader
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader");
+  loader.style.display = "none";
+});
